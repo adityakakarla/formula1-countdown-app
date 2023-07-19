f26d21d03d3039e52e4aa6b6c3fe494f63031a77
adityakakarla <85147239+adityakakarla@users.noreply.github.com>
2023-07-18 17:31:13 -0700

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Countdown from './components/countdown';
+import Standings from './components/standings';
+
+function App() {
+  return (
+    <div>
+      <Countdown/>
+      <Standings/>
+    </div>
+  );
+}
+
+export default App;
